4f8709e352f7d055607d4f44e28a13828c1d4804
Jon Bierma <jon.bierma@gmail.com>
2017-09-13 19:38:48 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,32 @@
+
+function forloop(arr){
+  for (let i=0; i<25;i++){
+    if (i===1){
+      arr[i]=`I am {$i} strange loop`
+    } else {
+    arr[i]=`I am {$i} strange loop`
+    }
+  }
+}
+
+function whileLoop(num){
+  while (num>=0){
+    console.log(num)
+    num--
+  }
+  console.log("done")
+}
+
+function doWhileLoop(arr){
+  do {
+    if (arr.length>0)
+      arr.pop()
+  } while (maybeTrue());
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+ 
+// run until `maybeTrue()` returns `false`
+// (so the body of the loop might _never_ run!)
